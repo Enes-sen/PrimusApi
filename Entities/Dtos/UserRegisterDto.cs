e0221ad7c0f6afd5a404c3718c2d8ae723739821
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-09-15 15:57:02 +0300

    Update UserRegisterDto.cs

--- Entities/Dtos/UserRegisterDto.cs
+++ Entities/Dtos/UserRegisterDto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ namespace Entities.Dtos
 {
     public class UserRegisterDto
     {
-        public required string UserName { get; set; }
-        public required string Password { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
