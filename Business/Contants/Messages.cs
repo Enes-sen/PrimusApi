09d907ce709fb43df3519df195e5631c9201355d
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-09-15 12:17:58 +0300

    Create Messages.cs

---
+++ Business/Contants/Messages.cs
@@ -0,0 +1,18 @@
+namespace Business.Contants
+{
+    public class Messages
+    {
+        //product mesages
+        public static readonly string AlienAdded = "Dna Örneği Başarıyla Eklendi!";
+        public static readonly string AlienDeleted = "Dna Örneği Başarıyla Kaldırıldı";
+
+        //user mesages
+        public static readonly string UserNotFound = "Kullanıcı Bulunamadı!";
+        public static readonly string PasswordError = "Hatalı Şifre!";
+        public static readonly string SuccessfullyLogged = "Giriş Başarılı!";
+        public static readonly string UserExists = "Bu Kullanıcı Zaten Var!";
+        public static readonly string UserRegistered = "Kullanıcı Başarıyla Kaydedildi!";
+        public static readonly string AccessTokenCreated = "Token Başarıyla oluşturuldu!";
+        public static readonly string PasswordHashSaltMissing = "some thing goes wrong!";
+    }
+}
