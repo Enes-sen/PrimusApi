e0ff6eb0c09fb4d2202bcc8f545a45b7c7ccda40
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-09-15 12:46:20 +0300

    Add files via upload

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    [Table("Users", Schema = "public")]
+    public class User : IEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [MaxLength(800)] // Kullanıcı adının maksimum uzunluğunu ayarlayabilirsiniz
+        public string? username { get; set; }
+
+        [Required]
+        public byte[]? passwordhash { get; set; }
+
+        [Required]
+        public byte[]? passwordsalt { get; set; }
+    }
+
+}
