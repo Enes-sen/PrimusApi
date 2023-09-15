f2874936c9cc23affe4b34fda7217db6946986c5
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-09-15 12:26:58 +0300

    Create DataResult.cs

---
+++ Core/Utilities/Result/DataResult.cs
@@ -0,0 +1,19 @@
+using Business.Utilities.Result;
+
+namespace Core1.Utilities.Results
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data, bool success, string message) : base(success, message)
+        {
+            Data = data;
+        }
+
+        public DataResult(T data, bool success) : base(success)
+        {
+            Data = data;
+        }
+
+        public T Data { get; }
+    }
+}
